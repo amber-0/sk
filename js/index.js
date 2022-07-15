377f0651cdf6ea719a3e01572039c64d3cb507e2
amber-0 <105370349+amber-0@users.noreply.github.com>
2022-07-16 07:43:54 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,19 +2,19 @@
 // 1~6の画像を表示する
 var pics_src = new Array
   ("https://github.com/amber-0/sk/blob/main/img/main1.gif?raw=true",
-  "img/main2.gif",
-  "img/main3.gif",
-  "img/main4.gif",
-  "img/main5.gif",
-  "img/main6.gif",
-  "img/main7.gif",
-  "img/main8.gif",
-  "img/main9.gif",
-  "img/main10.gif",
-  "img/main11.gif",
-  "img/main12.gif",
-  "img/main13.gif",
-  "img/main14.gif");
+  "https://github.com/amber-0/sk-/blob/main/img/main2.gif?raw=true",
+  "https://github.com/amber-0/sk-/blob/main/img/main3.gif?raw=true",
+  "https://github.com/amber-0/sk-/blob/main/img/main4.gif?raw=true",
+  "https://github.com/amber-0/sk-/blob/main/img/main5.gif?raw=true",
+  "https://github.com/amber-0/sk-/blob/main/img/main6.gif?raw=true",
+  "https://github.com/amber-0/sk-/blob/main/img/main7.gif?raw=true",
+  "https://github.com/amber-0/sk-/blob/main/img/main8.gif?raw=true",
+  "https://github.com/amber-0/sk-/blob/main/img/main9.gif?raw=true",
+  "https://github.com/amber-0/sk-/blob/main/img/main10.gif?raw=true",
+  "https://github.com/amber-0/sk-/blob/main/img/main11.gif?raw=true",
+  "https://github.com/amber-0/sk-/blob/main/img/main12.gif?raw=true",
+  "https://github.com/amber-0/sk-/blob/main/img/main13.gif?raw=true",
+  "https://github.com/amber-0/sk-/blob/main/img/main14.gif?raw=true");
   var num = 0;
 
   function go_forward(){
