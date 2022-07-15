50edc549131bfa9a60a25b10dd36a1c3ff20338e
amber-0 <105370349+amber-0@users.noreply.github.com>
2022-07-16 07:32:14 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
 
 // 1~6の画像を表示する
 var pics_src = new Array
-  ("/img/main1.gif",
-  "/img/main2.gif",
-  "/img/main3.gif",
-  "/img/main4.gif",
-  "/img/main5.gif",
-  "/img/main6.gif",
-  "/img/main7.gif",
-  "/img/main8.gif",
-  "/img/main9.gif",
-  "/img/main10.gif",
-  "/img/main11.gif",
-  "/img/main12.gif",
-  "/img/main13.gif",
-  "/img/main14.gif");
+  ("img/main1.gif",
+  "img/main2.gif",
+  "img/main3.gif",
+  "img/main4.gif",
+  "img/main5.gif",
+  "img/main6.gif",
+  "img/main7.gif",
+  "img/main8.gif",
+  "img/main9.gif",
+  "img/main10.gif",
+  "img/main11.gif",
+  "img/main12.gif",
+  "img/main13.gif",
+  "img/main14.gif");
   var num = 0;
 
   function go_forward(){
